ceb4c8bde576e7551c231da2636ba79c8bda0996
Filip <you@example.com>
2018-11-07 15:11:40 +0100

    nothing

--- src/components/main/Medical/Icons.js
+++ src/components/main/Medical/Icons.js
@@ -8,38 +8,38 @@ import style from '../Holding/styleHol.css';
 
 
 const Icons = () =>
-    (
-        <div>
-            <Grid style={{ margin: '0px' }}>
-                <GridRow style={{ background: 'white', paddingTop: '80px' }} centered>
-                    <GridColumn textAlign='center' className={style.flatDiv} computer={3} tablet={7} mobile={12}>
-                        <div className={style.flatBack}>
-                            <img src={medal} />
-                            <h3>QUALITY</h3>
-                        </div>
-                    </GridColumn>
-                    <GridColumn className={style.flatDiv} textAlign='center' computer={3} tablet={7} mobile={12}>
-                        <div className={style.flatBack}>
-                            <img src={services} />
-                            <h3>SERVICES</h3>
-                        </div>
-                    </GridColumn>
-                    <GridColumn className={style.flatDiv} textAlign='center' computer={3} tablet={7} mobile={12}>
-                        <div className={style.flatBack}>
-                            <img src={doctor} />
-                            <h3>VALUE</h3>
-                        </div>
-                    </GridColumn>
-                    <GridColumn className={style.flatDiv} textAlign='center' computer={3} tablet={7} mobile={12}>
-                        <div className={style.flatBack}>
-                            <img src={discount} />
-                            <h3>ATTRACTIVE PRICES</h3>
-                        </div>
-                    </GridColumn>
-                </GridRow>
-            </Grid>
-        </div>
-    )
+	(
+		<div>
+			<Grid style={{ margin: '0px' }}>
+				<GridRow style={{ background: 'white', paddingTop: '80px' }} centered>
+					<GridColumn textAlign='center' className={style.flatDiv} computer={3} tablet={7} mobile={12}>
+						<div className={style.flatBack}>
+							<img src={medal} />
+							<h3>QUALITY</h3>
+						</div>
+					</GridColumn>
+					<GridColumn className={style.flatDiv} textAlign='center' computer={3} tablet={7} mobile={12}>
+						<div className={style.flatBack}>
+							<img src={services} />
+							<h3>SERVICES</h3>
+						</div>
+					</GridColumn>
+					<GridColumn className={style.flatDiv} textAlign='center' computer={3} tablet={7} mobile={12}>
+						<div className={style.flatBack}>
+							<img src={doctor} />
+							<h3>VALUE</h3>
+						</div>
+					</GridColumn>
+					<GridColumn className={style.flatDiv} textAlign='center' computer={3} tablet={7} mobile={12}>
+						<div className={style.flatBack}>
+							<img src={discount} />
+							<h3>ATTRACTIVE PRICES</h3>
+						</div>
+					</GridColumn>
+				</GridRow>
+			</Grid>
+		</div>
+	)
 
 
 export default Icons;
